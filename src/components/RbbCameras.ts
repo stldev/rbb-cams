082767b04d31dcb47cf743c2594b2dcfd7e5b749
rbb <rick1stl@gmail.com>
2022-01-23 11:47:16 -0600

    filepath change

--- src/components/RbbCameras.ts
+++ src/components/RbbCameras.ts
@@ -1,7 +1,7 @@
 import { LitElement, html, css } from 'lit';
 import { property, customElement } from 'lit/decorators.js';
 
-const logo = new URL('../../assets/open-wc-logo.svg', import.meta.url).href;
+const logo = new URL('../../../assets/open-wc-logo.svg', import.meta.url).href;
 
 @customElement('rbb-cameras')
 export class RbbCameras extends LitElement {
